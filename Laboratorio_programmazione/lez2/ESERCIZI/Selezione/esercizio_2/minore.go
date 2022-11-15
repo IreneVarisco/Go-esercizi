f8ff3ff62b99eb2c4abbf5aa586ebdcb18d6e20f
Irene Varisco <73111808+IreneVarisco@users.noreply.github.com>
2022-11-15 11:01:44 +0100

    commenti

--- Laboratorio_programmazione/lez2/ESERCIZI/Selezione/esercizio_2/minore.go
+++ Laboratorio_programmazione/lez2/ESERCIZI/Selezione/esercizio_2/minore.go
@@ -6,9 +6,9 @@ func main() {
 
 	var a, b, c int
 	var m int
-
+	//scannerizzo i valori inseriti a terminale e li assegno alle 3 variabili
 	fmt.Scan(&a, &b, &c)
-
+	//identifico e stampo il valore più piccolo
 	if a < b {
 		if a < c {
 			m = a
