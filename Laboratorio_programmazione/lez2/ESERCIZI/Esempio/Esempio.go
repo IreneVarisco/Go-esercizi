94f0b62df906517ad4ada4431295e068739a4f74
Irene Varisco <73111808+IreneVarisco@users.noreply.github.com>
2022-11-15 10:55:49 +0100

    comments

--- Laboratorio_programmazione/lez2/ESERCIZI/Esempio/Esempio.go
+++ Laboratorio_programmazione/lez2/ESERCIZI/Esempio/Esempio.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Esempio")
+	//assegno alla variabile isco di tipo int un valore
 	var isco int = 10
+	//stampo una string concatenata col valore della varisbile isco
 	fmt.Println("Il valore della variabile è:", isco)
+	//assegno un nuovo valore ad isco
 	isco = 12
+	//stampo
 	fmt.Println("Il nuovo valore della variabile è:", isco)
 }
