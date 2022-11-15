b17bd5bfdad8c6d52083c57b579ab45e1be4159f
Irene Varisco <73111808+IreneVarisco@users.noreply.github.com>
2022-11-15 10:59:16 +0100

    commenti

--- Laboratorio_programmazione/lez2/ESERCIZI/Selezione/esercizio_1/somme.go
+++ Laboratorio_programmazione/lez2/ESERCIZI/Selezione/esercizio_1/somme.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	var (
+		//assegno a 2 diverse variabili 2 diversi valori
 		a, b int = 10, 20
 		c int = 30
 	)
+	//se a è maggiore di b, a assume il valore di b, altrimenti b assume quello di a
 	if a > b {
 		a = b
 	} else {
