cfbd103d2a50a4141f705725ec53574b4d5c3b52
Irene Varisco <73111808+IreneVarisco@users.noreply.github.com>
2022-11-17 08:35:33 +0100

    Update cerchio.go

--- Laboratorio_programmazione/lez2/ESERCIZI/Variabili_e_IO/esercizio_7/cerchio.go
+++ Laboratorio_programmazione/lez2/ESERCIZI/Variabili_e_IO/esercizio_7/cerchio.go
@@ -5,7 +5,7 @@ import "fmt"
 
 func main() {
 	var isco float64
-
+	//a seconda dell'intero inserito, stampa i dati di un cerchio che come raggio ha l'input
 	fmt.Println("Inserire il raggio del cerchio")
 	fmt.Scan(&isco)
 	fmt.Print("Raggio: ", isco, "\n",
