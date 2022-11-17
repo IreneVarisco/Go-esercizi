0d1f7488de1b558d6064fec9dd2525a9b1a452ac
Irene Varisco <73111808+IreneVarisco@users.noreply.github.com>
2022-11-17 08:47:53 +0100

    Update quadrato.go

--- Laboratorio_programmazione/lez03/quadrato.go
+++ Laboratorio_programmazione/lez03/quadrato.go
@@ -4,15 +4,18 @@ import "fmt"
 
 func main() {
 	var isco int
-	
+	//input lato quadrato
 	fmt.Scan(&isco)
-	for i:= 0; i <= isco; i++ {
-		for j:=0; j <= isco; j++ {
+	for i:= 0; i < isco; i++ {
+		for j:=0; j < isco; j++ {
 			if i == j {
+				//stampo diagonale
 				fmt.Print("o")
 			} else if i > j {
+				//stampa area sopra la diagonale
 				fmt.Print("*")
 			} else {
+				//stampa area sotto la diagonale
 				fmt.Print("+")
 			}
 		}
