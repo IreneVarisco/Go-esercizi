bd4abcd25e30d7dab7d36b345cba005e2534906c
Irene Varisco <73111808+IreneVarisco@users.noreply.github.com>
2022-11-17 09:06:42 +0100

    Update cicli.go

--- Laboratorio_programmazione/lez03/cicli.go
+++ Laboratorio_programmazione/lez03/cicli.go
@@ -6,24 +6,25 @@ func main() {
 	var isco int
 	var s, min, max, pos, neg, nul int
 
-	fmt.Println("inserire un numero")
+	fmt.Println("inserire numero di input")
 	fmt.Scan(&isco)
 
 	for i := 0; i < isco; i++ {
 		var a int
-
+		fmt.Println("inserire valore")
 		fmt.Scan(&a)
+		//somma
 		s += a
-
+		//valore più piccolo
 		if i == 0 || a < min {
 			min = a
 		}
 
-
+		//valore più grande
 		if i == 0 || a > max {
 			max = a
 		}
-
+		//controllo se il valore è maggiore, minore o uguale a 0
 		if a < 0 {
 			neg++
 		} else if a == 0 {
@@ -32,7 +33,7 @@ func main() {
 			pos++
 		}
 	}
-
+	//stampo tutti i dati
 	fmt.Print("somma: ", s, "\n",
 		"minimo: ", min, "\n",
 		"massimo: ", max, "\n",
